ccadfb4aa1000c3dbc19475e9c58c312064c01f9
ssterrett <scott.sterrett@alleninstitute.org>
2026-05-02 15:08:55 +0000

    Deploying to gh-pages from @ ssterrett/ssterrett.github.io@f8c432efa141f67937c57dc662764e7042c06eb4 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ssterrett/al-folio-site",
+    "data-repo": "ssterrett/ssterrett.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
